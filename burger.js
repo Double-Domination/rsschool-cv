da37545416b7d4baf9ede6111ce25a85cce48562
Double-Domination <squremeta@gmail.com>
2021-09-22 17:28:38 +0700

    feat: reserching how to implement burger menu pt1

---
+++ burger.js
@@ -0,0 +1,36 @@
+console.log('Burger dash');
+('use strict');
+
+const isMobile = {
+  android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  blackberry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  ios: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  windows: function () {
+    return navigator.userAgent.match(/IE Mobile/i);
+  },
+
+  any: function () {
+    return (
+      isMobile.android() ||
+      isMobile.blackberry() ||
+      isMobile.ios() ||
+      isMobile.opera() ||
+      isMobile.windows()
+    );
+  },
+};
+
+if (isMobile.any() === true) {
+  document.body.classList.add('_touch');
+} else {
+  document.body.classList.add('_pc');
+}
